3184bcc212a50a549a23c0afa5e7579c31dfc02b
Jose Faleiro <faleiro.jose.manuel@gmail.com>
2013-02-11 16:39:13 -0500

    Fix stack overflow in dependency list generation

--- db-simulation.py
+++ db-simulation.py
@@ -14,12 +14,98 @@ class G:
     
     # Dependency graph specific stuff
 
-    Roots = []                              # The first frontier of txs to materialize
+    Roots = set([])                         # The first frontier of txs to materialize
     LastWrite = {}                          # The last writer of a tuple
     TxMap = {}                              # Map each tranasction to an integer    
-    NumIOs = 0                              # Total #IOs used during materialization
+
     DependencyGraph = nx.DiGraph()          # The actual dependency graph
+
+    NumIOs = 0                              # Total #IOs used during materialization
     NumMaterialized = 0                     # The #TXs materialized
+
+    ReadIOs = 0                             # Total IOs used for materializing reads
+    NumReads = 0                            # # Materializing reads
+
+    Record = -1
+    
+
+class GenRead(SimPy.Simulation.Process):
+    ReadRate = 1/0.1
+    
+    def __init__(self):
+        SimPy.Simulation.Process.__init__(self)
+        
+    def BackwardsBFS(q, done):
+        if not q:
+            return done
+
+        elif q[0] in done:
+            del q[0]
+            return GenRead.BackwardsBFS(q, done)
+        
+        else:
+            q = q + G.DependencyGraph.predecessors(q[0])
+            done.append(q[0])
+            del q[0]
+            return GenRead.BackwardsBFS(q, done)
+        
+    BackwardsBFS = staticmethod(BackwardsBFS)
+
+    def Read(n):        
+        tot = 0
+
+        if n in G.LastWrite:
+            txList = GenRead.BackwardsBFS([G.LastWrite[n]], [])
+            txList.reverse()
+            print txList
+            done = []
+            inMem = []
+            while txList:
+                if txList[0] in done:
+                    continue
+
+                done.append(txList[0])
+                records = G.TxMap[txList[0]]
+                diff = set(records) - set(inMem)
+                tot += 2*len(diff)                
+                inMem += records
+                tx = txList[0]
+                del txList[0]
+                
+                assert tx in G.Roots
+
+                G.Roots.remove(tx)                    
+                assert not (tx in G.Roots)                
+                G.Roots.update(G.DependencyGraph.successors(tx))
+
+                G.DependencyGraph.remove_node(tx)                
+
+                # Update all last write information: In case this was
+                # the last write of a record, update the info.
+                for record in records:
+                    if G.LastWrite[record] == tx:
+                        G.LastWrite.pop(record)
+                
+        else:
+            tot += 1
+
+        G.ReadIOs += tot
+        G.NumReads += 1
+
+    Read = staticmethod (Read)
+    
+    def GenerateRead():
+        hot = range(0, G.NumHot)
+        temp = random.choice(hot)
+        return  temp
+        
+    GenerateRead = staticmethod (GenerateRead)
+
+    def Run(self):
+        while 1:
+            record = GenRead.GenerateRead()
+            GenRead.Read(record)
+            yield SimPy.Simulation.hold, self, G.Rnd.expovariate(GenRead.ReadRate)
 
 
 # This class is used to generate transactions in the
@@ -34,14 +120,14 @@ class G:
 # this will directly affect the contention index (from Calvin).
 
 class GenTx(SimPy.Simulation.Process):
-    TxRate = 1/0.01
+    TxRate = 1/0.001
     TP = 0
     HP = 0
     CP = 0
     
-    def __init__(self, db):
+    def __init__(self):
         SimPy.Simulation.Process.__init__(self)
-        self.DB = db
+#        self.DB = db
 
     def GenTransaction():
         # First set the globally unique TxNo. This reads from
@@ -54,7 +140,7 @@ class GenTx(SimPy.Simulation.Process):
         
         # Generate the read set. This is done in the same 
         # manner as the Calvin paper.
-        for i in range(0, GenTx.NumRecords):
+        for i in range(0, G.NumRecords):
             
             # The first record is always a hot record. 
             # We have a pointer to hot record Identifiers (HP).
@@ -62,14 +148,14 @@ class GenTx(SimPy.Simulation.Process):
             # to generate the hot record of the subsequent tx.
             if i == 0:
                 retTx.append(GenTx.HP)
-                GenTx.HP = (GenTx.HP+1) % GenTx.NumHot
+                GenTx.HP = (GenTx.HP+1) % G.NumHot
 
             # All the other records are cold records. 
             # The first cold record is one greater than the
             # last hot record. 
             else:
                 retTx.append(GenTx.CP)
-                GenTx.CP = ((GenTx.CP+1) % GenTx.NumCold) + GenTx.NumHot
+                GenTx.CP = ((GenTx.CP+1) % G.NumCold) + G.NumHot
 
         ret['Tx'] = retTx
         G.TxMap[ret['TxNo']] = retTx
@@ -98,7 +184,7 @@ class GenTx(SimPy.Simulation.Process):
             
         if isRoot:
             assert isinstance(t['TxNo'], int)
-            G.Roots.append(t['TxNo'])
+            G.Roots.add(t['TxNo'])
     
     InsertTx = staticmethod(InsertTx)
     
@@ -106,62 +192,33 @@ class GenTx(SimPy.Simulation.Process):
         while 1:
             curTx = GenTx.GenTransaction()
             GenTx.InsertTx(curTx)
-            SimPy.Simulation.reactivate(self.DB)
+#            SimPy.Simulation.reactivate(self.DB)
             yield SimPy.Simulation.hold, self, G.Rnd.expovariate(GenTx.TxRate)
-                
-class Materialize(SimPy.Simulation.Process):
-
-    def __init(self):
-        SimPy.Simulation.Process.__init__(self)
-
-    # This method processes all roots in FIFO order.    
-    def FIFORoot():
-        if not G.Roots:
-            return False
-        else:
-            t = G.Roots[0]
-            del G.Roots[0]
-            G.NumIOs += 2*(GenTx.NumRecords)
-            G.Roots = G.Roots+G.DependencyGraph.successors(t)
-            G.DependencyGraph.remove_node(t)
-            
-            # Update all last write information: In case this was
-            # the last write of a record, update the info.
-            for record in G.TxMap[t]:
-                if G.LastWrite[record] == t:
-                    G.LastWrite.pop(record)
-            G.NumMaterialized += 1
-            return True
-
-    FIFORoot = staticmethod(FIFORoot)
-
-    def Run(self):
-        while 1:
-            if not Materialize.FIFORoot():
-                yield SimPy.Simulation.passivate, self
             
 
 def main():
     # Initialize the static members of the transaction generation class.
-    GenTx.NumHot = 1000
-    GenTx.NumCold = 1000000
-    GenTx.NumRecords = 10
+    G.NumHot = 1000
+    G.NumCold = 1000000
+    G.NumRecords = 10
 
     # Initialize the static members of the materialization class.
     # Materialize.Process = Materialize.FIFORoot
     
     SimPy.Simulation.initialize()    
     
-    db = Materialize()
-    tx = GenTx(db)
+    rtx = GenRead()
+    tx = GenTx()
     
-    SimPy.Simulation.activate(db, db.Run())
-    SimPy.Simulation.activate(tx, tx.Run())
+#    SimPy.Simulation.activate(db, db.Run())
 
+    SimPy.Simulation.activate(tx, tx.Run())
+    SimPy.Simulation.activate(rtx, rtx.Run())
     MaxSimTime = 1000.0
     SimPy.Simulation.simulate(until = MaxSimTime)
 
-    print G.NumIOs/G.NumMaterialized            
+    print G.ReadIOs / G.NumReads
+
                 
         
 if __name__ == '__main__':
