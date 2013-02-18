61ef00820c06d1b49f322cad9a1e30862e7a6b42
Jose Faleiro <faleiro.jose.manuel@gmail.com>
2013-02-17 23:02:32 -0500

    Keep track of the longest tail

--- db-simulation.py
+++ db-simulation.py
@@ -17,6 +17,7 @@ class G:
 
     Roots = set([])                         # The first frontier of txs to materialize
     LastWrite = {}                          # The last writer of a tuple
+    MetaData = []
     TxMap = {}                              # Map each tranasction to an integer    
 
     DependencyGraph = nx.DiGraph()          # The actual dependency graph
@@ -43,76 +44,106 @@ class GenRead(SimPy.Simulation.Process):
         done = []
         
         while queue:
-            if queue[0] in done:
-                done.append(queue[0])
-                del queue[0]
-            else:
+            if not(queue[0] in done):
                 queue = queue + G.DependencyGraph.predecessors(queue[0])
                 done.append(queue[0])
-                del queue[0]
+                
+            del queue[0]
         return done
             
         
     BackwardsBFS = staticmethod(BackwardsBFS)
 
-    def Read(n):        
+
+
+    def Read(n, isFree):        
         tot = 0
         numMat = 0
         if n in G.LastWrite:
             
-            
-            txList = GenRead.BackwardsBFS(G.LastWrite[n]['Tx'])
-            txList.sort()
-            #print txList
-            done = {}
-            inMem = []
-            while txList:
-                if txList[0] in done:
-                    del txList[0]
-                    continue
-                
-                #print 1
-                records = G.TxMap[txList[0]]['Tx']
-                inMem += records
-                tx = txList[0]
+            # Since we've already stored the records that will
+            # be materialized as a result of this transaction in the
+            # LastWrite dictionary, we don't have to do anything more
+            # to figure out the number of IOs for this materialization.
+            #
+            # However, we still have to update the graph to reflect the
+            # dependency structure properly.
+            tx = G.LastWrite[n]['Tx']
 
+            # From this point onwards, everything is oriented towards
+            # updating information in the dependency graph.
+            txList = GenRead.BackwardsBFS(tx)
+            
+            # It is safe to execute transactions in sorted order because
+            # there are no unknown dependencies.
+            txList.sort()
+
+            done = set([])
+            
+            inMem = []
+
+            # Iterates through the list of transactions that are executed
+            # as a result of this materialization.            
+            for tx in txList:
                 
+                # The transaction has already been processed. This can happen
+                # because we don't remove duplicates from the list and two
+                # different transactions could be dependent on the same transaction.
+                if tx in done:
+                    continue
+
+                # Print this for debugging purposes. The only case in which this
+                # stuff will print out is if the assertion that follows 
+                # is false.
+                #
+                # The invariant that we're maintaining is that the transactions are
+                # being processed in topologically sorted order. This means that in
+                # order to be able to execute a transaction, it must not be dependent
+                # on any other transaction, ie, it must be a root.
                 if not(tx in G.Roots):
-                    #print done
-                    for t in done:
-                        print G.DependencyGraph.predecessors(t)
-                        print G.DependencyGraph.successors(t)
-                        print 'blah'
-                        
+
                     print G.DependencyGraph.predecessors(tx)
                     print G.DependencyGraph.successors(tx)
                     print txList
                     print tx
 
                 assert tx in G.Roots
-                done[tx] = -1
-                del txList[0]
 
-                G.Roots.remove(tx)                    
-                assert not (tx in G.Roots)                
-                G.Roots.update(set(G.DependencyGraph.successors(tx)))
-
-                G.DependencyGraph.remove_node(tx)                
+                children = G.DependencyGraph.successors(tx)
+                G.DependencyGraph.remove_node(tx)  
                 
-                # Update all last write information: In case this was
-                # the last write of a record, update the info.
+                done.add(tx)
+                G.Roots.remove(tx)                    
+                assert not (tx in G.Roots)               
 
+                for child in children:
+                    if not G.DependencyGraph.predecessors(child):
+                        G.Roots.add(child)
+                
+                records = G.TxMap[tx]
+                
                 for record in records:
-
                     if G.LastWrite[record]['Tx'] == tx:
                         del G.LastWrite[record]
+                    else:
+                        G.LastWrite[record]['IO'] -= 1;
                     
-            numMat = len(set(done.keys()))
-            tot = 2*len(set(inMem))
+                inMem += G.TxMap[tx]
+                
+                
+            if not isFree:
+                tot = 2 * len(set(inMem))
+            else:
+                tot = 0
+
+                    
+            numMat = len(done)
+            print 'end'
                 
         else:
             tot = 1
             
+        print G.DependencyGraph.nodes()
         #print numMat
         G.NumMaterialized += numMat
         G.ReadIOs += tot
@@ -130,10 +161,17 @@ class GenRead(SimPy.Simulation.Process):
         
     GenerateRead = staticmethod (GenerateRead)
 
+    def RunBackground(self):
+        while 1:
+            if G.MetaData:
+                record = G.MetaData[0]
+                GenRead.Read(record, True)
+            yield SimPy.Simulation.hold, self, G.Rnd.expovariate(GenRead.BgMatRate)
+
     def Run(self):
         while 1:
             record = GenRead.GenerateRead()
-            GenRead.Read(record)
+            GenRead.Read(record, False)
             yield SimPy.Simulation.hold, self, G.Rnd.expovariate(GenRead.ReadRate)
         
 
@@ -199,7 +237,7 @@ class GenTx(SimPy.Simulation.Process):
                 retTx.append(coldItem)
 
         ret['Tx'] = retTx
-        G.TxMap[ret['TxNo']] = ret
+        G.TxMap[ret['TxNo']] = retTx
         return ret
 
     GenTransaction = staticmethod(GenTransaction)
@@ -210,10 +248,12 @@ class GenTx(SimPy.Simulation.Process):
         
         G.DependencyGraph.add_node(index)
 
-        recordSet = t['Tx']
-        
+        recordSet = list(t['Tx'])
+        print len(t['Tx'])
+
+        cnt = 0
         for record in t['Tx']:
-            
+            cnt += 1
             # Check if there is a tx that writes to this record.
             #
             # If yes, we have to add a dependency between this tx
@@ -221,31 +261,51 @@ class GenTx(SimPy.Simulation.Process):
 
             if record in G.LastWrite:
                 isRoot = False                
-                assert (G.LastWrite[record]['Tx'] != index)
-                
+                assert (G.LastWrite[record]['Tx'] != index)       
+                assert G.LastWrite[record]['Tx'] in G.DependencyGraph.nodes()
+
                 G.DependencyGraph.add_edge(G.LastWrite[record]['Tx'], index)
-                parentTx = G.TxMap[G.LastWrite[record]['Tx']]
-                #print parentTx
-                recordSet = recordSet | parentTx['recordSet']            
+                G.LastWrite[record]['Tx'] = index
+                G.LastWrite[record]['IO'] += 1
+            else:
+                G.LastWrite[record] = {}
+                G.LastWrite[record]['Tx'] = index
+                G.LastWrite[record]['IO'] = 1
             
-        t['recordSet'] = recordSet
-        
-        for record in t['Tx']:
-            G.LastWrite[record] = {}
-            G.LastWrite[record]['Tx'] = index
-            G.LastWrite[record]['IO'] = len(recordSet)
+            GenTx.UpdateMeta(record)
 
         if isRoot:
-            assert isinstance(t['TxNo'], int)
-            assert not(t['TxNo'] in G.Roots)
-            G.Roots.add(t['TxNo'])
-    
+            G.Roots.add(t['TxNo'])        
+        
+
+                
     InsertTx = staticmethod(InsertTx)
+
+    def UpdateMeta(record):
+        # Try to remove the record from the sorted list, if we fail, it's ok.
+        try:
+            G.MetaData.remove(record)
+        except:
+            pass
+        
+        # Find the new count of the record.
+        count = G.LastWrite[record]['IO']
+        length = len(G.MetaData)
+
+        # Search for the right position and insert it.
+        for i in range(0, length):
+            curCount = G.LastWrite[G.Metadata[i]]['IO']
+            if curCount < count:
+                G.MetaData.insert(i, record)
+                break
+            
+    UpdateMeta = staticmethod(UpdateMeta)
     
     def Run(self):
         while 1:
             curTx = GenTx.GenTransaction()
             GenTx.InsertTx(curTx)
+            
             yield SimPy.Simulation.hold, self, G.Rnd.expovariate(GenTx.TxRate)
 
 
@@ -262,14 +322,17 @@ def main():
     # Materialize.Process = Materialize.FIFORoot    
     
     GenRead.ReadRate = 1 / float(sys.argv[1])
+    GenRead.BgMatRate = 1 / float(sys.argv[2])
     SimPy.Simulation.initialize()
     
     rtx = GenRead()
     tx = GenTx()
+    bg = GenRead()
 
     SimPy.Simulation.activate(tx, tx.Run())
     SimPy.Simulation.activate(rtx, rtx.Run())
-    MaxSimTime = 10.0
+    SimPy.Simulation.activate(bg, bg.RunBackground())
+    MaxSimTime = 100.0
     SimPy.Simulation.simulate(until = MaxSimTime)
     
     rResults = open('reads.txt', 'a')
